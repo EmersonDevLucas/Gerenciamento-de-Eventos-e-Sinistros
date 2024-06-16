91db5f9b139886c82594f568370529c1e4978906
Emerson Souza <emersonlucasts@gmail.com>
2024-06-16 16:45:18 -0300

    Update script.js

--- menu/script.js
+++ menu/script.js
@@ -2,3 +2,7 @@ document.getElementById('gestao-nav').onclick = function() {
     window.location.replace('https://emersondevlucas.github.io/Gerenciamento-de-Eventos-e-Sinistros/menu/')
 }
 
+document.getElementById('dashboard-nav').onclick = function() {
+    window.location.replace('https://emersondevlucas.github.io/Gerenciamento-de-Eventos-e-Sinistros/dashboard/')
+}
+
